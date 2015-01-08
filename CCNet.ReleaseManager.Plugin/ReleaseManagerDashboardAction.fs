ab158dd55d06838d5871136520f2f7ac27312b02
Janno Põldma <janno.poldma@gmail.com>
2015-01-08 17:26:07 +0200

    Dashboard view template.

---
+++ CCNet.ReleaseManager.Plugin/ReleaseManagerDashboardAction.fs
@@ -0,0 +1,11 @@
+﻿namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ReleaseManager
+
+open System.Collections
+open ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise
+open ThoughtWorks.CruiseControl.WebDashboard.MVC.View
+
+type ReleaseManagerDashboardAction (viewGenerator: IVelocityViewGenerator) =
+    static member val ActionName = "ReleaseManagerDashboard" with get
+    interface ICruiseAction with
+        override __.Execute(cruiseRequest) =
+            upcast viewGenerator.GenerateView("ReleaseManagerDashboard.vm", Hashtable())
--- CCNet.ReleaseManager.Plugin/ReleaseManagerPlugin.fs
+++ CCNet.ReleaseManager.Plugin/ReleaseManagerPlugin.fs
@@ -1,10 +1,17 @@
 ﻿namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ReleaseManager
 
 open Exortech.NetReflector
+open System.Collections
 open ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+open ThoughtWorks.CruiseControl.WebDashboard.MVC
+open ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise
+open ThoughtWorks.CruiseControl.WebDashboard.MVC.View
 
 [<ReflectorType("ReleaseManagerPlugin")>]
-type ReleaseManagerPlugin (actionInstantiator: IActionInstantiator) =
+type ReleaseManagerPlugin (actionInstantiator: IActionInstantiator, viewGenerator: IVelocityViewGenerator) =
     interface IPlugin with
         override val LinkDescription = "Release Manager" with get
-        override val NamedActions = [| ImmutableNamedAction("HelloWorld", actionInstantiator.InstantiateAction(typeof<HelloWorldAction>)) :> INamedAction |] with get
+        override val NamedActions = [|
+                ImmutableNamedAction(ReleaseManagerDashboardAction.ActionName,
+                                     actionInstantiator.InstantiateAction(typeof<ReleaseManagerDashboardAction>)) :> INamedAction
+            |] with get
