af6168e1be855b5836a0d27850909bffb6f7888e
Janno Põldma <janno.poldma@gmail.com>
2015-01-08 17:29:49 +0200

    Cleanup.

--- CCNet.ReleaseManager.Plugin/ReleaseManagerPlugin.fs
+++ CCNet.ReleaseManager.Plugin/ReleaseManagerPlugin.fs
@@ -1,17 +1,13 @@
 ﻿namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ReleaseManager
 
 open Exortech.NetReflector
-open System.Collections
 open ThoughtWorks.CruiseControl.WebDashboard.Dashboard
-open ThoughtWorks.CruiseControl.WebDashboard.MVC
-open ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise
-open ThoughtWorks.CruiseControl.WebDashboard.MVC.View
 
 [<ReflectorType("ReleaseManagerPlugin")>]
-type ReleaseManagerPlugin (actionInstantiator: IActionInstantiator, viewGenerator: IVelocityViewGenerator) =
+type ReleaseManagerPlugin (actionInstantiator: IActionInstantiator) =
+    let dashboardAction = actionInstantiator.InstantiateAction(typeof<ReleaseManagerDashboardAction>)
     interface IPlugin with
         override val LinkDescription = "Release Manager" with get
         override val NamedActions = [|
-                ImmutableNamedAction(ReleaseManagerDashboardAction.ActionName,
-                                     actionInstantiator.InstantiateAction(typeof<ReleaseManagerDashboardAction>)) :> INamedAction
+                ImmutableNamedAction(ReleaseManagerDashboardAction.ActionName, dashboardAction) :> INamedAction
             |] with get
