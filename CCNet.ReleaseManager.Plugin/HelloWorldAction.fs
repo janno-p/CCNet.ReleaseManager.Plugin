84e45d15b3b3e895e4fe31f32a72f2c5275893d3
Janno Põldma <janno.poldma@gmail.com>
2015-01-08 15:14:58 +0200

    Testing CCNet WebDashboard plugin infrastructure.

---
+++ CCNet.ReleaseManager.Plugin/HelloWorldAction.fs
@@ -0,0 +1,9 @@
+﻿namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ReleaseManager
+
+open ThoughtWorks.CruiseControl.WebDashboard.MVC
+open ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise
+
+type HelloWorldAction () =
+    interface ICruiseAction with
+        override __.Execute(cruiseRequest) =
+            upcast HtmlFragmentResponse("<h1>Hello, World!</h1>")
---
+++ CCNet.ReleaseManager.Plugin/ReleaseManagerPlugin.fs
@@ -0,0 +1,10 @@
+﻿namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ReleaseManager
+
+open Exortech.NetReflector
+open ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+
+[<ReflectorType("ReleaseManagerPlugin")>]
+type ReleaseManagerPlugin (actionInstantiator: IActionInstantiator) =
+    interface IPlugin with
+        override val LinkDescription = "Release Manager" with get
+        override val NamedActions = [| ImmutableNamedAction("HelloWorld", actionInstantiator.InstantiateAction(typeof<HelloWorldAction>)) :> INamedAction |] with get
